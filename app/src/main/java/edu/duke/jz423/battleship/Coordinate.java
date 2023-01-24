5c1efa80a0fa7741360a9bf999f5bf6ca20ccf94
jz423 <jz423@duke.edu>
2023-01-23 23:34:12 -0500

    task3 finish

--- app/src/main/java/edu/duke/jz423/battleship/Coordinate.java
+++ app/src/main/java/edu/duke/jz423/battleship/Coordinate.java
@@ -4,6 +4,30 @@ public class Coordinate {
   private final int row;
   private final int column;
 
+
+
+  /**
+     This constructor receive a string, whose length is 2, the first char is for the row, the second one is for the column
+     @param descr: the string
+     the row is 0 - 9, column is A - Z, both lowercase and uppercase are acceptable
+   */
+  public Coordinate(String descr) {
+    if (descr.length() != 2) {
+      throw new IllegalArgumentException("Invalid size of string!");
+    }
+
+    char rowLetter = Character.toUpperCase(descr.charAt(0));
+    char columnLetter = descr.charAt(1);
+    if (rowLetter < 'A' || rowLetter > 'Z') {
+      throw new IllegalArgumentException("Invalid input for row alphabet, must be less then U!");
+    }
+    if (columnLetter < '0' || columnLetter > '9') {
+      throw new IllegalArgumentException("Invalid input for column number, must between 0 and 9!");
+    }
+    this.column = Character.getNumericValue(columnLetter);
+    this.row = rowLetter - 'A';
+  }
+
   public Coordinate(int r, int c) {
     this.row = r;
     this.column = c;
