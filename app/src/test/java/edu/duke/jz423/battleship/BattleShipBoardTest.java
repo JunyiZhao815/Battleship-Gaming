27d6eca5bae85abd1f19f0aa0d238862bd8399ad
jz423 <jz423@duke.edu>
2023-01-26 19:09:10 -0500

    goal3 finish

--- app/src/test/java/edu/duke/jz423/battleship/BattleShipBoardTest.java
+++ app/src/test/java/edu/duke/jz423/battleship/BattleShipBoardTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -21,21 +20,21 @@ public class BattleShipBoardTest {
   @Test
   public void test_invalid_dimensions() {
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(10, 0));
+        () -> new BattleShipBoard<Character>(10, 0));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(0, 0));
+        () -> new BattleShipBoard<Character>(0, 0));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(-8, 0));
+        () -> new BattleShipBoard<Character>(-8, 0));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(0, 20));
+        () -> new BattleShipBoard<Character>(0, 20));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(-8, 20));
+        () -> new BattleShipBoard<Character>(-8, 20));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(0, -20));
+        () -> new BattleShipBoard<Character>(0, -20));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(10, -5));
+        () -> new BattleShipBoard<Character>(10, -5));
     assertThrows(IllegalArgumentException.class,
-                 () -> new BattleShipBoard<Character>(-8, -20));
+        () -> new BattleShipBoard<Character>(-8, -20));
 
   }
 
@@ -63,13 +62,13 @@ public class BattleShipBoardTest {
     Placement p = new Placement(c, 'V');
     Ship<Character> ship = shipFactory.createShip(p, 1, 3, 'c', null);
     assertTrue(board.tryAddShip(ship));
-    
+
     Ship<Character> ship2 = shipFactory.createShip(new Placement(new Coordinate(2, 1),
         'V'), 2, 2, 'c', null);
     assertFalse(board.tryAddShip(ship2));
-        Ship<Character> ship3 = shipFactory.createShip(new Placement(new Coordinate(2, 2),
+    Ship<Character> ship3 = shipFactory.createShip(new Placement(new Coordinate(2, 2),
         'V'), 2, 2, 'c', null);
-        assertTrue(board.tryAddShip(ship3));
+    assertTrue(board.tryAddShip(ship3));
   }
 
   @Test
@@ -86,8 +85,6 @@ public class BattleShipBoardTest {
     assertThrows(IllegalArgumentException.class, () -> checkWhatIsAtBoard(b1, e3));
     checkWhatIsAtBoard(b1, e5);
 
-
-    
     Coordinate c = new Coordinate(1, 1);
     RectangleShip<Character> s = new RectangleShip<Character>(c, 's', '*');
 
